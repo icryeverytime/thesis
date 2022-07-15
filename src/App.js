bcc4db879e2ef92e4e2a9d07d61972a0cdd56a99
icryeverytime <al242348@edu.uaa.mx>
2022-07-15 12:05:06 -0500

    Mongodb Atlas support added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Navbar from './navbar/navindex';
 import Home from './home/homeindex';
 import Login from './login/loginindex';
 import SignUp from './signup/SignUpindex';
-import {HashRouter as Router,Routes,Route} from 'react-router-dom';
+import {HashRouter,Router,Routes,Route} from 'react-router-dom';
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home/>}/>
         <Route path="thesis/Login" element={<Login/>}/>
         <Route path="thesis/SignUp" element={<SignUp/>}/>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 export default App;
